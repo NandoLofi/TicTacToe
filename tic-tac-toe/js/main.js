b40a6c44f505d964e3dd753081c87a04f7c72e09
Fernando Galvan <n.brighter00@gmail.com>
2022-03-15 21:34:04 -0500

    2nd commit

---
+++ tic-tac-toe/js/main.js
@@ -0,0 +1,10 @@
+console.log('JS is working')
+/*----- constants -----*/
+
+/*----- app's state (variables) -----*/
+
+/*----- cached element references -----*/
+
+/*----- event listeners -----*/
+
+/*----- functions -----*/
