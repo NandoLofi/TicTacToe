ea341d92c1a5b6b7e6f0241c0c220c9b3d97a851
Fernando Galvan <n.brighter00@gmail.com>
2022-03-15 22:28:41 -0500

    added reset button and styling

--- tic-tac-toe/js/main.js
+++ tic-tac-toe/js/main.js
@@ -1,9 +1,20 @@
 console.log('JS is working')
 /*----- constants -----*/
+//X and O - Player
+//Winning combos
+//
 
 /*----- app's state (variables) -----*/
+//where the users click
+//Bonus - scores for who won
+//whose turn it is
+//when we have winning combo or draw
+//if the player wants to resign  - bonus
+//timer - bonus
 
 /*----- cached element references -----*/
+// the games squares
+//reset button
 
 /*----- event listeners -----*/
 
